e745e27bc54eb7de9fee768bbb4f668dda0388f5
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-04-19 11:07:51 -0500

    Initial commit

---
+++ lib/Alien/Adaptagrams.pm
@@ -0,0 +1,20 @@
+package Alien::Adaptagrams;
+# ABSTRACT: Alien package for the Adaptagrams adaptive diagram library
+
+use strict;
+use warnings;
+
+use parent qw(Alien::Base);
+
+1;
+__END__
+
+=head1 Inline support
+
+This module supports L<Inline's with functionality|Inline/"Playing 'with' Others">.
+
+=head1 SEE ALSO
+
+L<Adaptagrams|http://www.adaptagrams.org/>
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Alien-Adaptagrams/>
