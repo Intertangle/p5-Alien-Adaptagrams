473d2087635c80fc1b0128d1685d1a4f1aa47aca
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-04-23 18:39:53 -0500

    Add pkg_config_path and static linking Inline

--- lib/Alien/Adaptagrams.pm
+++ lib/Alien/Adaptagrams.pm
@@ -6,6 +6,44 @@ use warnings;
 
 use parent qw(Alien::Base);
 
+sub pkg_config_path {
+	my ($class) = @_;
+	if( $class->install_type eq 'share' ) {
+		return File::Spec->catfile( File::Spec->rel2abs($class->dist_dir), qw(lib pkgconfig) );
+	} else {
+		return ExtUtils::PkgConfig->variable('libcola', 'pcfiledir');
+	}
+}
+
+sub Inline {
+	my ($self, $lang) = @_;
+
+	my @libs = qw(avoid vpsc dialect topology cola);
+	if( $lang =~ /^CPP$/ ) {
+		my $params = Alien::Base::Inline(@_);
+
+		# Use static linking instead of dynamic linking. This works
+		# better on some platforms. On macOS, to use dynamic linking,
+		# the `install_name` of the library must be set, but since this
+		# is the final path by default, linking to the `.dylib` under
+		# `blib/` at test time does not work without using `@rpath`.
+		if( $^O eq 'darwin' and $self->install_type eq 'share' ) {
+			$params->{MYEXTLIB} .= ' ' .
+				join( " ",
+					map { File::Spec->catfile(
+						File::Spec->rel2abs(Alien::Adaptagrams->dist_dir),
+						'lib',  "lib$_.a" ) }
+					@libs
+				);
+			$params->{LIBS} =~ s/-l$_//g for @libs;
+		}
+
+		$params->{LIBS} = join " ", qw(:nosearch), $params->{LIBS}, qw(:search);
+
+		return $params;
+	}
+}
+
 1;
 __END__
 
